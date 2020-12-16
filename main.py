f3f87145cc9c89327bcffc0b79b4fb5b33436435
Samsul Hoque Choudhury <sam45564@gmail.com>
2020-12-16 23:35:26 +0530

    Added time elapsed calculation, fixed sitemap urlset attributes.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from crawler.scraper import scrape
 from xml_generator.create import create
 from xml_generator.save import save
+from datetime import timedelta
 
 # Add the website for which sitemap will be generated.
-seed_url = ""
+seed_url = "https://whenabongcooks.com/"
 
 
 print("Scraping started...\n")
+start = timedelta()
 data = scrape(seed_url)
 
 print("Scraping completed...\n")
@@ -19,4 +21,6 @@ print(f"Last link: {data[total_links - 1]}\n\n")
 print("Creating sitemap.xml\n")
 xml = create(data)
 save(xml)
+end = timedelta()
 print("Created sitemap.xml\n\n")
+print(f"Time elapsed: {end - start}")
--- xml_generator/create.py
+++ xml_generator/create.py
@@ -7,6 +7,7 @@ def create(url_list):
     """Return prettify XML string for the given 'url_list'
     """
     document_attributes = {
+        'xmlns': 'http://www.sitemaps.org/schemas/sitemap/0.9',
         'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
         'xsi:schemaLocation': 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'
     }
